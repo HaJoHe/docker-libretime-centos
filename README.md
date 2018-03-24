9f2f00d70b4078fbe2a9401d8f2e3050beafe464
dd8ne <dd8ne@Lapi_alt.local>
2018-03-24 16:28:50 +0100

    update postgresql pg_hba.conf

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # DOCKER Container for running libretime (www.libretime.org)
 
-# How to run? 
-docker run -p 80:80 -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
- -d --privileged \
- --name libretime \ 
- hajo/docker-libretime-centos
+## How to run? 
+docker run -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
+ -v /tmp/$(mktemp -d):/run \
+ -v /srv/libretime_centos/stor:/srv/airtime/stor \
+ -v /srv/libretime_centos/watch:/srv/airtime/watch \
+ -v /srv/libretime_centos/postgresql:/var/lib/pgsql/data \
+ -v /srv/libretime_centos/etc:/etc/airtime \
+ -p 80:80 -p 8000-8005:8000-8005 --name libretime \
+ -d hajo/docker-libretime-centos
 
-# Initialisierung
+### Initialisierung
 docker exec -it libretime /1stRun.sh
 
 If installation has finished, do a config of libretime via Web
--- help/1stRun.sh
+++ help/1stRun.sh
@@ -11,9 +11,10 @@ if [ ! -f $FILE ]; then
    #
    echo "Init database"
    # modify ownership for mounted data area
-   chown postgresql:postgresql /var/lib/pgsql/data
+   chown postgres:postgres /var/lib/pgsql/data
    postgresql-setup initdb
-   echo "host    all             all             127.0.0.1/32 md5" >  /var/lib/pgsql/data/pg_hba.conf
+   echo "local   all             all             peer"             >  /var/lib/pgsql/data/pg_hba.conf
+   echo "host    all             all             127.0.0.1/32 md5" >> /var/lib/pgsql/data/pg_hba.conf
    echo "host    all             all             ::0/128      md5" >> /var/lib/pgsql/data/pg_hba.conf
    systemctl enable postgresql
    systemctl restart postgresql
@@ -40,4 +41,5 @@ if [ ! -f $FILE ]; then
    cd /libretime_src/libretime
    ./install -fiap
 
+   touch $FILE
 fi
