22455c2aae4351ecef0ba7f7a147b98eb23cd678
dd8ne <dd8ne@Lapi_alt.local>
2018-03-24 10:43:34 +0100

    1stRUN to init librtime 3nd try

--- help/1stRun.sh
+++ help/1stRun.sh
@@ -5,26 +5,39 @@ FILE=/1st_start.done
 #test if already done
 if [ ! -f $FILE ]; then
    echo "1st Run ---- Init"
+
+   #
+   # postgresql
+   #
    echo "Init database"
+   # modify ownership for mounted data area
+   chown postgresql:postgresql /var/lib/pgsql/data
    postgresql-setup initdb
-   echo "local   all             all             peer		> /var/lib/pgsql/data/pg_hba.conf
-   echo "host    all             all             0.0.0.0/0 trust" >>  /var/lib/pgsql/data/pg_hba.conf
-   echo "host    all             all             127.0.0.1/32 md5" >> /var/lib/pgsql/data/pg_hba.conf
+   echo "host    all             all             127.0.0.1/32 md5" >  /var/lib/pgsql/data/pg_hba.conf
    echo "host    all             all             ::0/128      md5" >> /var/lib/pgsql/data/pg_hba.conf
-   #echo "listen_addresses='*'" >> /var/lib/pgsql/data/pg_hba.conf
    systemctl enable postgresql
    systemctl restart postgresql
-
-   #rabbitmq
+   #
+   # rabbitmq
+   #
    systemctl enable rabbitmq-server
    systemctl restart rabbitmq-server
-   
+   #
+   # icecast
+   #
+   systemctl enable icecast
+   #
    # php
    echo "update dateformat for php"
    echo "date.timezone = CET" > /etc/php.d/datetime.ini
+   #
+   # httpd
+   #
+   systemctl enable httpd
+   systemctl restart httpd
 
    echo "install libretime please be patient ....."
    cd /libretime_src/libretime
-   HOME=/root
    ./install -fiap
+
 fi
